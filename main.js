dd71ece85e46e5b61abe4fe6e070a3dd2bbfac03
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-22 15:47:58 +0100

    aggiunti valori età e km da percorrere

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const km =prompt('digita quanti km devi percorrere');
-const eta =prompt('digita quanti anni hai');
+let km = prompt('digita quanti km devi percorrere');
+let eta = prompt('digita quanti anni hai');
+let costante = 0.21;
 let risultato; 
 /* converto stringa in numero intero */
-km= parseint(km);
-eta = parseint(eta);
+ /*Stampo in html i chilometri e l'età*/
+document.getElementById("eta").innerHTML = eta;
+document.getElementById("km").innerHTML = km;
+
  
-
-
