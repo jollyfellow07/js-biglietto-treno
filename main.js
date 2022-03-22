370b2d66ebd7fa6f98666e1e9e2db4330a97837c
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-22 14:29:48 +0100

    conversione stringe in numeri

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const km =prompt('digita quanti km devi percorrere');
 const eta =prompt('digita quanti anni hai');
-
+let risultato; 
+/* converto stringa in numero intero */
+km= parseint(km);
+eta = parseint(eta);
+ 
 
 
