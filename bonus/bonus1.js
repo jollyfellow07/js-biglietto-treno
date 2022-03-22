3b0c67e85c0b172855d9722b1f2889ca44b0a9bb
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-22 17:45:41 +0100

    AGGIUNTI COMMENTI

--- bonus/bonus1.js
+++ bonus/bonus1.js
@@ -1,15 +1,18 @@
 let persona1 = prompt('inserisci l eta di Simone');
 let persona2 = prompt('inserisci l eta di Francesco');
-
+/* converto stringa in numero intero */
 parseInt (persona1);
 parseInt(persona2);
 
+/* SE SIMONE E' PIU GRANDE DI FRANCESCO */
 if (persona1>persona2){
     alert ('Simone è più grande di Francesco')
 }
+/* SE FRANCESCO E' PIU GRANDE DI SIMONE */
 else if (persona2>persona1){
     alert ('Francesco è più grande di Simone')
 }
+/* SE AVETE LA STESSA ETA */
 else{
     alert('Simone e Francesco hanno la stessa età')
 }
--- bonus/bonus2.js
+++ bonus/bonus2.js
@@ -1,23 +1,26 @@
 alert('Benvenuto nel gioco pari o dispari')
 alert('il sistema sceglierà un numero e se il numero che avrai scelto sarà della stessa tipologia di quello che ha scelto il sistema avrai vinto! :D');
 let numero= prompt('scegli un numero da 1 a 9');
-let sistema = Math.floor((Math.random()*9) + 1);
-parseInt(numero);
 
+/* converto stringa in numero intero */
+parseInt(numero);
+/* GENERO NUMERO RANDOM DA 1 A 9 */
+let sistema = Math.floor((Math.random()*9) + 1);
 alert('il sistema ha scelto il numero: ');
 alert(sistema);
-
+/* CONDIZIONE NUMERO PARI */
 if((sistema==2 || sistema==4 || sistema==6 || sistema==8) && (numero==2 || numero==4 || numero==6 || numero==8)){
 
-    alert('hai vinto avete scelto entrambi un numero pari');
+    alert('hai vinto, avete scelto entrambi un numero pari :D ');
 
  }
-
+/* CONDIZIONE NUMERO DISPARI */
  else if((sistema==1 || sistema==3 || sistema==5 || sistema==7 || sistema==9) && (numero==1 || numero==3 || numero==5 || numero==7 || numero==9)) {
 
-    alert('hai vinto avete scelto entrambi un numero dispari');
+    alert('hai vinto, avete scelto entrambi un numero dispari :D ');
 
  }
+ /* CONDIZIONE SE AVETE SCELTO NUMERI DI TIPOLOGIA DIVERSA */
     else {
-        alert('non hai vinto avete scelto due numeri di tipologia diversa');
+        alert('non hai vinto :(... avete scelto due numeri di tipologia diversa');
     }
