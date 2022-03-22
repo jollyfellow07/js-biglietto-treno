7a2fddc4d828302a35a5bb4ba93f3ca20c49c0ef
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-22 15:54:14 +0100

    aggiunta conversione interi

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ let eta = prompt('digita quanti anni hai');
 let costante = 0.21;
 let risultato; 
 /* converto stringa in numero intero */
+parseInt (km);
+parseInt(eta);
  /*Stampo in html i chilometri e l'età*/
 document.getElementById("eta").innerHTML = eta;
 document.getElementById("km").innerHTML = km;
+
+if (){
+
+}
 
  
